ff5090a79341c1ba6885ec1426782378765d077d
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-03-26 01:27:51 +0000

    add horizental scroling

---
+++ components/Sponsors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sponsors = () => {
+  return (
+    <section id='sponsors' className='h-screen w-screen flex justify-center items-center'>
+        Sponsors
+    </section>
+  )
+}
+
+export default Sponsors
