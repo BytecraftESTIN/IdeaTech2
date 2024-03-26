6e99a95e0e5b581f5eb97f47213329b8c7a6a110
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-03-26 01:50:43 +0000

    add horizentale scroling

---
+++ components/Agenda.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Agenda = () => {
+  return (
+    <section id='agenda' className='h-screen w-screen flex justify-center items-center'>
+      Agenda
+    </section>
+  )
+}
+
+export default Agenda
